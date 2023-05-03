3f860484305ff48b1360def7aafa6e1744437aa0
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-05-03 19:49:25 +0300

    ConnectionPoolConfig added

--- src/main/java/by/smirnov/newsproject/NewsProjectApplication.java
+++ src/main/java/by/smirnov/newsproject/NewsProjectApplication.java
@@ -1,9 +1,14 @@
 package by.smirnov.newsproject;
 
+import by.smirnov.newsproject.configuration.ConnectionPoolConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@SpringBootApplication(scanBasePackages = "by.smirnov.newsproject")
+@Import({ConnectionPoolConfig.class})
 public class NewsProjectApplication {
 
 	public static void main(String[] args) {
