bdd27f9e3a902a101e9a917811118b6dd5f62eee
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-05-06 20:03:04 +0300

    Comment entity added

--- src/main/java/by/smirnov/newsproject/domain/Comment.java
+++ src/main/java/by/smirnov/newsproject/domain/Comment.java
@@ -1,12 +1,11 @@
 package by.smirnov.newsproject.domain;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,17 +18,17 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "news")
-public class News {
+@Table(name = "comment")
+public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private Timestamp time;
-    private String title;
     private String text;
+    private String username;
 
-    @OneToMany(mappedBy = "newsItem", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
-    private List<Comment> comments;
-
+    @ManyToOne
+    @JoinColumn(name = "news_id")
+    private News newsItem;
 }
