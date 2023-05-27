9bad2b46356bcf71c90e2e327f242123fc9e2cde
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-05-27 23:05:58 +0300

    DTO updated

--- src/main/java/by/smirnov/newsproject/dto/NewsResponse.java
+++ src/main/java/by/smirnov/newsproject/dto/NewsResponse.java
@@ -1,4 +1,12 @@
 package by.smirnov.newsproject.dto;
 
+import by.smirnov.newsproject.domain.Comment;
+
+import java.sql.Timestamp;
+import java.util.List;
+
 public class NewsResponse extends NewsRequest{
+
+    private long id;
+    private List<Comment> comments;
 }
