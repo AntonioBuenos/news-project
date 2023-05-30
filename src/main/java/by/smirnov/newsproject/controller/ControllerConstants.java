cee661312aa1111539de3f8b9b204e5d169b3421
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-05-30 21:53:55 +0300

    ControllerConstants added

---
+++ src/main/java/by/smirnov/newsproject/controller/ControllerConstants.java
@@ -0,0 +1,18 @@
+package by.smirnov.newsproject.controller;
+
+public interface ControllerConstants {
+
+    String MAPPING_ID = "/{id}";
+
+    String MAPPING_NEWS = "/news";
+
+    String MAPPING_COMMENTARIES = "/commentaries";
+
+    String ID = "id";
+
+    String TAGS = "news";
+
+    String CERTIFICATES = "commentaries";
+
+    String DELETED = "Object by this ID successfully deleted";
+}
--- src/main/java/by/smirnov/newsproject/dto/CommentResponse.java
+++ src/main/java/by/smirnov/newsproject/dto/CommentResponse.java
@@ -2,8 +2,6 @@ package by.smirnov.newsproject.dto;
 
 import by.smirnov.newsproject.domain.News;
 
-import java.sql.Timestamp;
-
 public class CommentResponse extends CommentRequest{
 
     private long id;
