da67c33eba09dea6a58dd3174edd2a646985563d
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-05-31 21:37:53 +0300

    NewsController added

---
+++ src/main/java/by/smirnov/newsproject/controller/NewsController.java
@@ -0,0 +1,96 @@
+package by.smirnov.newsproject.controller;
+
+import by.smirnov.newsproject.domain.News;
+import by.smirnov.newsproject.dto.NewsRequest;
+import by.smirnov.newsproject.dto.NewsResponse;
+import by.smirnov.newsproject.exception.BadRequestException;
+import by.smirnov.newsproject.exception.NoSuchEntityException;
+import by.smirnov.newsproject.service.NewsService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static by.smirnov.newsproject.controller.ControllerConstants.DELETED;
+import static by.smirnov.newsproject.controller.ControllerConstants.ID;
+import static by.smirnov.newsproject.controller.ControllerConstants.MAPPING_ID;
+import static by.smirnov.newsproject.controller.ControllerConstants.MAPPING_NEWS;
+import static javax.print.attribute.standard.ReferenceUriSchemesSupported.NEWS;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(MAPPING_NEWS)
+public class NewsController {
+
+    private final NewsService service;
+
+    private final NewsConverter converter;
+
+    @GetMapping
+    public ResponseEntity<Map<String, List<NewsResponse>>> index() {
+        List<NewsResponse> responses = service.findAll()
+                .stream()
+                .map(converter::convert)
+                .toList();
+        return new ResponseEntity<>(Collections.singletonMap(NEWS, responses), HttpStatus.OK);
+    }
+
+    @GetMapping(MAPPING_ID)
+    public ResponseEntity<NewsResponse> show(@PathVariable(ID) long id) {
+
+        News tag = service.findById(id);
+        NewsResponse response = converter.convert(tag);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<NewsResponse> create(@RequestBody @Valid NewsRequest request,
+                                              BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            throw new BadRequestException(ValidationErrorConverter.getErrors(bindingResult).toString());
+        }
+
+        News tag = converter.convert(request);
+        News created = service.create(tag);
+        NewsResponse response = converter.convert(created);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping(MAPPING_ID)
+    public ResponseEntity<NewsResponse> update(@PathVariable(name = ID) Long id,
+                                              @RequestBody @Valid NewsRequest request,
+                                              BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            throw new BadRequestException(ValidationErrorConverter.getErrors(bindingResult).toString());
+        }
+
+        News tag = converter.convert(request);
+        News changed = service.update(tag);
+        NewsResponse response = converter.convert(changed);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping(MAPPING_ID)
+    public ResponseEntity<Map<String, Long>> delete(@PathVariable(ID) long id) {
+
+        if (Objects.isNull(service.findById(id))) throw new NoSuchEntityException();
+        service.delete(id);
+        return new ResponseEntity<>(Map.of(DELETED, id), HttpStatus.OK);
+    }
+}
