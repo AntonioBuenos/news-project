ab341d66b510c1eb3fc20f50e3acf2cc072ba409
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-06-03 22:22:56 +0300

    Validation and controller constants added

---
+++ src/main/java/by/smirnov/newsproject/controller/CommonControllerConstants.java
@@ -0,0 +1,10 @@
+package by.smirnov.newsproject.controller;
+
+import static by.smirnov.newsproject.controller.ControllerConstants.ID;
+
+public interface CommonControllerConstants {
+
+    String PAGE_SORT = ID;
+    int PAGE_SIZE = 20;
+    String CODE = "code";
+}
---
+++ src/main/java/by/smirnov/newsproject/validation/ValidationConstants.java
@@ -0,0 +1,21 @@
+package by.smirnov.newsproject.validation;
+
+public interface ValidationConstants {
+
+    String NOT_BLANK_MESSAGE = "This value cannot be empty or blank";
+    String INVALID_ENUM_MESSAGE = "Invalid Enum value";
+    String INVALID_COUNTRY_MESSAGE = "Country not found";
+    int STANDARD_MIN_SIZE = 2;
+    int STANDARD_MAX_SIZE = 20;
+    String STANDARD_SIZE_MESSAGE = "This value length must be between {min} and {max} characters long. Your input is: ${validatedValue}";
+    int PASSPORT_MAX_SIZE = 10;
+    String MAX_SIZE_MESSAGE = "This value length must be up to {max} characters long. Your input is: ${validatedValue}";
+    int PASSWORD_MIN_SIZE = 2;
+    int PASSWORD_MAX_SIZE = STANDARD_MAX_SIZE;
+    int ADDRESS_MAX_SIZE = 50;
+    int EXTENDED_MAX_SIZE = 30;
+    String NOT_NULL_MESSAGE = "This value cannot be null";
+    String NOT_NEGATIVE_MESSAGE = "This value cannot be negative";
+    String EMAIL_MESSAGE = "Email must match email format";
+    String ERROR = "Error";
+}
