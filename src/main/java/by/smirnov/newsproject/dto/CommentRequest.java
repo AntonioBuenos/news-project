ea400ce2f9d02fb882afe3b06fd28c68ef41304c
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-06-08 23:37:49 +0300

    Validation constraints added

--- src/main/java/by/smirnov/newsproject/dto/CommentRequest.java
+++ src/main/java/by/smirnov/newsproject/dto/CommentRequest.java
@@ -1,6 +1,7 @@
 package by.smirnov.newsproject.dto;
 
-import by.smirnov.newsproject.domain.News;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +11,13 @@ import java.sql.Timestamp;
 @Setter
 public class CommentRequest {
 
+    @PastOrPresent
     private Timestamp time;
+
+    @NotBlank
     private String text;
+
+    @NotBlank
     private String username;
 
 }
