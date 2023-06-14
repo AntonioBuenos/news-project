a565123b537f8810d5cbd47b8399dd0cdf49f230
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-06-14 22:12:41 +0300

    Springdoc config added

--- src/main/java/by/smirnov/newsproject/NewsProjectApplication.java
+++ src/main/java/by/smirnov/newsproject/NewsProjectApplication.java
@@ -1,6 +1,7 @@
 package by.smirnov.newsproject;
 
 import by.smirnov.newsproject.configuration.ConnectionPoolConfig;
+import by.smirnov.newsproject.configuration.OpenAPIConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -8,7 +9,7 @@ import org.springframework.context.annotation.Import;
 
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @SpringBootApplication(scanBasePackages = "by.smirnov.newsproject")
-@Import({ConnectionPoolConfig.class})
+@Import({ConnectionPoolConfig.class, OpenAPIConfig.class})
 public class NewsProjectApplication {
 
 	public static void main(String[] args) {
